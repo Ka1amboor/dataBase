7c5de901cd135a14aef933283d76d43af09c5645
Ka1amboor <120525868+Ka1amboor@users.noreply.github.com>
2024-10-11 16:36:00 +0300

    Update requests.sql

--- lab2/requests.sql
+++ lab2/requests.sql
@@ -24,3 +24,23 @@ CREATE TABLE Contracts (
     data DATE,
     total_sum FLOAT
 );
+
+--
+
+-- Заполнение таблицы клиентов
+INSERT INTO Clients (client_id, name, surname, address, phone_number, email) VALUES
+(1, 'Иван', 'Воскресенский', 'Россия, Санкт-Петербуррг, ул. Стремянная, 1', '+79525525252', 'ivanov@example.com'),
+(2, 'Петр', 'Петров', 'Россия, Санкт-Петербург, ул. Пушкина, 2', '+79522223344', 'petrov@example.com');
+
+-- Заполнение таблицы финансовых продуктов
+INSERT INTO FinancialProducts (product_id, type, conditions, bet) VALUES
+(1, 'Кредиты', 'Срок до 5 лет, ставка 10%', 10.0),
+(2, 'Депозиты', 'Ставка 5% годовых', 5.0),
+(3, 'Страхование', 'Застраховать автомобиль или имущество', 3.0);
+
+-- Заполнение таблицы договоров
+INSERT INTO Contracts (contract_id, client_id, product_id, data, total_sum) VALUES
+(1, 1, 1, '2023-01-01', 5000000.00),
+(2, 1, 2, '2023-02-01', 1000000.00),
+(3, 2, 1, '2023-03-01', 3000000.00),
+(4, 2, 3, '2023-04-01', 200000.00);
